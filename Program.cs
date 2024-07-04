437d2eda48cb750eb785dcc7d98a191a355af7f3
fmings <mingsfelicia@gmail.com>
2024-07-04 08:57:42 -0500

    setup

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Compression;
+
+List<Plant> plants = new List<Plant>()
+    new Plant()
+    {
+        Species = "bush",
+        LightNeeds = 3,
+        AskingPrice = 15.99M,
+        City = "Louisville",
+        ZIP = 40108,
+        Sold = true
+    }
+    new Plant()
+    {
+        Species = "flower",
+        LightNeeds = 3,
+        AskingPrice = 18.99M,
+        City = "Lexington",
+        ZIP = 40258,
+        Sold = true
+    }
+    new Plant()
+    {
+        Species = "bean",
+        LightNeeds = 1,
+        AskingPrice = 5.99M,
+        City = "Brandenburg",
+        ZIP = 40175,
+        Sold = false
+    }
+    new Plant()
+    {
+        Species = "grass",
+        LightNeeds = 3,
+        AskingPrice = 10.99M,
+        City = "Nashville",
+        ZIP = 37208,
+        Sold = false
+    }
+    new Plant()
+    {
+        Species = "tree",
+        LightNeeds = 5,
+        AskingPrice = 26.99M,
+        City = "Brentwood",
+        ZIP = 37027,
+        Sold = false
+    }
